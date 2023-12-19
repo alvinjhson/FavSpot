26386360b404d5076c5da0727f5f47d6f3ce3b52
alvinjhson <alvinbest26@gmail.com>
2023-12-19 11:02:41 +0100

    Auth

--- app/src/main/java/com/example/favspot/MainActivity.kt
+++ app/src/main/java/com/example/favspot/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.favspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
+    lateinit var emailView: EditText
+    lateinit var passwordView : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 }
