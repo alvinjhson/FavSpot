7127af73a6fcd85bba44714b33097052971b33b8
alvinjhson <alvinbest26@gmail.com>
2023-12-19 11:30:28 +0100

    log in screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.favspot"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.favspot"
---
+++ app/src/main/java/com/example/favspot/FavSpotactivity.kt
@@ -0,0 +1,11 @@
+package com.example.favspot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavSpotactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fav_spot)
+    }
+}
