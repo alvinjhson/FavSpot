cfae2d9442dbd1303b37674d69aedb700d79dcd2
alvinjhson <alvinbest26@gmail.com>
2023-12-27 16:07:00 +0100

    You can now rate the spot

--- app/src/main/java/com/example/favspot/SpotList.kt
+++ app/src/main/java/com/example/favspot/SpotList.kt
@@ -6,10 +6,12 @@ class SpotList() {
     var itemName: String = ""
     var itemImage: String = ""  // Ändra detta till ett lämpligt defaultvärde
     var id: String = ""
+    var rating:Float = 0.0f
 
-    constructor(itemName: String, itemImage: String, id: String) : this() {
+    constructor(itemName: String, itemImage: String, id: String,rating: Float) : this() {
         this.itemName = itemName
         this.itemImage = itemImage
         this.id = id
+        this.rating = rating
     }
 }
