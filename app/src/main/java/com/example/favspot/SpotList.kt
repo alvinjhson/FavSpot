82321f6122a61abd02bc7ce09b9791a0e1b726c3
alvinjhson <alvinbest26@gmail.com>
2024-01-02 16:14:05 +0100

    you can now write a description of your favspot
    s

--- app/src/main/java/com/example/favspot/SpotList.kt
+++ app/src/main/java/com/example/favspot/SpotList.kt
@@ -7,11 +7,13 @@ class SpotList() {
     var itemImage: String = ""  // Ändra detta till ett lämpligt defaultvärde
     var id: String = ""
     var rating:Float = 0.0f
+    var itemDesc: String = ""
 
-    constructor(itemName: String, itemImage: String, id: String,rating: Float) : this() {
+    constructor(itemName: String, itemImage: String, id: String,rating: Float,itemDesc: String) : this() {
         this.itemName = itemName
         this.itemImage = itemImage
         this.id = id
         this.rating = rating
+        this.itemDesc = itemDesc
     }
 }
